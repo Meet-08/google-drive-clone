9b9171dc6a76d7d63cfe89b1ba6b49e03bebf08d
Meet-08 <meetjbhuva@gmail.com>
2025-05-19 19:58:01 +0530

    Added layout for authentication

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <div className="bg-white h-screen">
+      <h1 className={"primary-btn"}>
+        StoreIt - The only storage solution you need
+      </h1>
+      <div className={"w-screen bg-blue h-20 shadow-drop-3"}></div>
+    </div>
+  );
+};
+
+export default Page;
